7e749c7ad78527b92870b47b0363487ab5553fea
Hannah Ingham <hannahingham@MacBooks-MacBook-Pro.local>
2019-03-11 16:15:35 -0700

    configure for deploy

--- src/pages/habitat/index.js
+++ src/pages/habitat/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Layout from "../../components/Layout"
+import Layout from "../../components/layout"
 import AccordionList from "../../components/accordion-list.js"
 import "../../components/h1.scss"
 
